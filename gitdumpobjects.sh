4c6d52cce6d3cbb808281f982dc9332a953fe721
Dan <danielgrinkevich@gmail.com>
2018-04-14 10:11:01 -0400

    Create gitdumpobjects.sh

---
+++ gitdumpobjects.sh
@@ -0,0 +1,9 @@
+#This script dumps all the git objects files, useful for CTFs
+#grep the output for the flag or just read it
+
+objects=$(find .git/objects -type f | sed 's/.git\/objects\///' | tr -d /)
+for obj in $objects
+  do
+    echo $obj
+    git cat-file -p $obj
+  done
